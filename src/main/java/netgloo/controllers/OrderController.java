c9aeafe6c3d6c229c9a7fb3cfbfeeef7997e1e57
adrikacang <adrianseptiandry@gmail.com>
2016-05-16 11:46:10 +0700

    no message

--- src/main/java/netgloo/controllers/OrderController.java
+++ src/main/java/netgloo/controllers/OrderController.java
@@ -47,7 +47,7 @@ public class OrderController {
         obj.put("ordernumber","Stock not ready, order not made");
         return obj;
       }
-   		int harga = sayur.getPrice();
+   		int harga = sayur.getPrice() * quantity;
       token = Long.toHexString(Double.doubleToLongBits(Math.random())).substring(0,5);
       System.out.println(pembeli.getEmail());
       System.out.println(sayur.getName());
