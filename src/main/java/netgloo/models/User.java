f910120a778536e2f4ab6b915a53666da80b2ec1
arisyaag <aghny.arisya@gmail.com>
2016-05-19 04:48:55 +0700

    Add unit test

--- src/main/java/netgloo/models/User.java
+++ src/main/java/netgloo/models/User.java
@@ -1,77 +1,41 @@
 package netgloo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.google.common.base.Objects;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Represents an User for this web application.
  */
+
 @Entity
-@Table(name = "users")
 public class User {
 
-  // ------------------------
-  // PRIVATE FIELDS
-  // ------------------------
-  
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
-  
   @NotNull
-  private String email;
-  
-  @NotNull
-  private String name;
+  @Size(max = 64)
+  @Column(name = "id", nullable = false, updatable = false)
+  private String id;
 
   @NotNull
+  @Size(max = 64)
+  @Column(name = "password", nullable = false)
   private String password;
 
-  //private Set<Order> orders;
-  // ------------------------
-  // PUBLIC METHODS
-  // ------------------------
-  
-  public User() { }
-
-  public User(long id) { 
-    this.id = id;
+  User() {
   }
 
-  public User(String email, String name, String password) {
-    this.email = email;
-    this.name = name;
+  public User(final String id, final String password) {
+    this.id = id;
     this.password = password;
   }
 
-  public long getId() {
+  public String getId() {
     return id;
   }
 
-  public void setId(long value) {
-    this.id = value;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-  
-  public void setEmail(String value) {
-    this.email = value;
-  }
-  
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String value) {
-    this.name = value;
-  }
-  
   public String getPassword() {
     return password;
   }
@@ -80,13 +44,11 @@ public class User {
     this.password = password;
   }
 
-
-  // @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-  // public Set<Order> getOrders() {
-  //     return orders;
-  // } 
-
-  // public void setOrders(Set<Order> value) {
-  //   this.orders = value;
-  // }
-} // class User
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this)
+            .add("id", id)
+            .add("password", password)
+            .toString();
+  }
+}
